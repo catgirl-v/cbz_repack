3bc34fd8fb9502be14292a01e47d7ae13acffd5f
novenary <streetwalkermc@gmail.com>
2023-05-13 11:09:39 +0300

    Add "How Do We Relationship?" by Tamifull

---
+++ works/how_do_we_relationship.py
@@ -0,0 +1,36 @@
+# https://anilist.co/manga/104241/How-Do-We-Relationship/
+# Nyaa source ID: 1628460
+
+volumes = [
+    'How Do We Relationship v01 (2020) (Digital) (1r0n) (f).cbz',
+    'How Do We Relationship v02 (2020) (Digital) (1r0n) (f).cbz',
+    'How Do We Relationship v03 (2021) (Digital) (1r0n) (f).cbz',
+    'How Do We Relationship v04 (2021) (Digital) (1r0n) (f).cbz',
+    'How Do We Relationship v05 (2022) (Digital) (1r0n).cbz',
+    'How Do We Relationship v06 (2022) (Digital) (1r0n).cbz',
+    'How Do We Relationship v07 (2022) (Digital) (1r0n).cbz',
+    'How Do We Relationship v08 (2023) (Digital) (1r0n).cbz',
+]
+
+details = {
+    "title": "How Do We Relationship?",
+    "author": "Tamifull",
+    "artist": "Tamifull",
+    "description": "Shy Miwa has always dreamed of finding love, but living in small-town Japan made finding the right match difficult—especially since she likes girls! Even going away to college didn’t seem to help, until one day her outgoing classmate Saeko suggests they might as well start dating each other since it’s not like either of them has other options.\n\nAt first it seems like things won't work out as their personalities clash and misunderstandings abound. But when their casual friendship starts to become something more, Miwa begins to wonder—can a pragmatic proposal lead to true love?",
+    "genre": [
+        "Drama",
+        "Romance",
+        "Slice of Life",
+    ],
+    "status": "1",
+}
+
+toc = {
+}
+
+page_regex = r"^How Do We Relationship - c(?P<chapter>[\d#]+) \(v\d\d\) - p[\dp-]+ \[VIZ Media] \[Digital] \[1r0n]\.(?:jpg|png)$"
+
+cover_volume = 1
+cover_file = "How Do We Relationship - c001 (v01) - p000 [VIZ Media] [Digital] [1r0n].jpg"
+
+subchapter_separator = "#"
