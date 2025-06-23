fbd480cafe6f95d10ea6401ac4f7b3a685e3191d
novenary <novenary@kwak.zip>
2025-06-23 12:46:26 +0300

    Add "Futari Escape" by Taguchi Shouichi

---
+++ works/futari_escape.py
@@ -0,0 +1,75 @@
+# https://anilist.co/manga/120067/Futari-Escape/
+# Nyaa source ID: 1777344
+
+volumes = [
+    "Futari Escape v01 (2022) (Digital) (LuCaZ).cbz",
+    "Futari Escape v02 (2023) (Digital) (LuCaZ).cbz",
+    "Futari Escape v03 (2023) (Digital) (LuCaZ).cbz",
+    "Futari Escape v04 (2023) (Digital) (LuCaZ).cbz",
+]
+
+details = {
+    "title": "Futari Escape",
+    "author": "Taguchi Shouichi",
+    "artist": "Taguchi Shouichi",
+    "description": "Two young women–one an overworked manga artist, the other a complete slacker–try to escape the realities of adult responsibilities. Whether it’s running off on a day trip instead of being productive or going into debt for an epic meal, the two always have fun when they’re together! Enjoy this delightful (and relatable) slice-of-life tale about the little pleasures in ducking away from the hard stuff.",
+    "genre": [
+        "Comedy",
+        "Slice of Life",
+    ],
+    "status": "2",
+}
+
+toc = {
+    # Volume 1
+    "1": "Escape Offline",
+    "2": "Escape to the Last Stop",
+    "3": "Escape to Decadence",
+    "4": "A Penniless Escape",
+    "5": "Escape to Hokuriku",
+    "6": "Escape to the Jurassic",
+    "7": "Cleaning Escape",
+    "8": "Escape into the Past",
+    "8.1": "Solo Escape",
+
+    # Volume 2
+    "9": "Escape Summertime",
+    "10": "Escape Summer's End",
+    "11": "Midnight Escape",
+    "12": "Escape Sleep",
+    "13": "Escape Old Age",
+    "14": "Locked Out Escape",
+    "15": "Farewell Escape",
+    "16": "Escape Like a Cloud",
+    "16.1": "Escape Blank Pages",
+
+    # Volume 3
+    "17": "Family Care Escape",
+    "18": "Cooking Escape",
+    "19": "Escape the Pounds",
+    "20": "No More Escaping",
+    "21": "Escape the Party",
+    "22": "Christmas Escape",
+    "22.5": "One More Escape",
+    "23": "Escape Tax Season",
+    "24": "Cherry Blossom Escape",
+
+    # Volume 4
+    "25": "Escape To Bed",
+    "26": "Escape with Chikuwabu",
+    "27": "Escape On Air",
+    "28": "Escape Good Vibes",
+    "29": "Emergency Escape",
+    "30": "Izu Ammonite Escape",
+    "31": "Escape Through Loss",
+    "32": "Futari Escape",
+}
+
+chapter_overrides = {}
+
+page_regex = r"^Futari Escape - c(?P<chapter>[\dx]+) \(v\d\d\) - p(?P<page>[\dp-]+) (?:\[(?:Cover|ToC|Afterword)\] )?\[dig\] \[Seven Seas\] \[LuCaZ\] {HQ}\.jpg$"
+
+cover_volume = 1
+cover_file = "Futari Escape - c001 (v01) - p000 [Cover] [dig] [Seven Seas] [LuCaZ] {HQ}.jpg"
+
+subchapter_separator = "x"
